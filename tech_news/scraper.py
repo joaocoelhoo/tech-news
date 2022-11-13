bde5d92f88569ad2dce65ca7699590b5165852db
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-11-13 17:25:08 -0300

    create scrape_next_page_link function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,7 +29,10 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_url = selector.css("a.next::attr(href)").get()
+
+    return next_url
 
 
 # Requisito 4
