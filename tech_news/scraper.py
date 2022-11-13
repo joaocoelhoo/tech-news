68254b77b54f3c26d44e53ef37f189d1fa1cef81
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-11-13 18:36:08 -0300

    create scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,7 +37,27 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head > link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get()
+    date = selector.css("li.meta-date::text").get()
+    author = selector.css("span.author > a::text").get()
+    comments = selector.css("ol.comment-list li").getall()
+    summary = selector.css(
+        "div.entry-content > p:first-of-type *::text").getall()
+    tags = selector.css("section.post-tags a::text").getall()
+    category = selector.css("div.meta-category span.label::text").get()
+    print(category)
+    return {
+        "url": url,
+        "title": title.strip(),
+        "timestamp": date,
+        "writer": author,
+        "comments_count": len(comments),
+        "summary": ''.join(summary).strip(),
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
